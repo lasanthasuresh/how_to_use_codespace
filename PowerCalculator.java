bdb2a1494771f13cd37ec0610d410592752d2aae
Pasan Kavinda Abeysekara <69195287+PasanAbeysekara@users.noreply.github.com>
2023-09-21 07:39:57 +0000

    Add comments to generate documentation for CalculatePower function Add appopriate white spaces Close the scanner to release resources

--- PowerCalculator.java
+++ PowerCalculator.java
@@ -12,14 +12,26 @@ public class PowerCalculator {
     double exponent = scanner.nextDouble();
 
     double power = calculatePower(base, exponent);
-    System.out.println("The power of " + base + " raised to" + exponent + " is " +power );
+
+    System.out.println("The power of " + base + " raised to " + exponent + " is " + power);
+
+    scanner.close();
   }
 
+  /**
+   * Calculates the power of a base number raised to an exponent.
+   * 
+   * @param base     The base number.
+   * @param exponent The exponent.
+   * @return The result of base^exponent.
+   */
   private static double calculatePower(double base, double exponent) {
     double result = 1;
+
     for (int i = 0; i < exponent; i++) {
       result *= base;
     }
+
     return result;
   }
 }
