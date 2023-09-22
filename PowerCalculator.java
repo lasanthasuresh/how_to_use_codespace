dc300eda19d84bf2e1d7868bf7a7aa7eb6559380
Piumikavindya <118907095+Piumikavindya@users.noreply.github.com>
2023-09-22 05:52:16 +0000

    first commit

--- PowerCalculator.java
+++ PowerCalculator.java
@@ -6,19 +6,19 @@ public class PowerCalculator {
     Scanner scanner = new Scanner(System.in);
 
     System.out.println("Enter the base number: ");
-    double base = scanner.nextDouble();
+    double baseNumber = scanner.nextDouble();
 
     System.out.println("Enter the exponent: ");
     double exponent = scanner.nextDouble();
 
-    double power = calculatePower(base, exponent);
-    System.out.println("The power of " + base + " raised to" + exponent + " is " +power );
+    double power = calculatePower(baseNumber, exponent);
+    System.out.println("The power of " + baseNumber + " raised to" + exponent + " is " +power );
   }
 
-  private static double calculatePower(double base, double exponent) {
+  private static double calculatePower(double baseNumber, double exponent) {
     double result = 1;
     for (int i = 0; i < exponent; i++) {
-      result *= base;
+      result *= baseNumber;
     }
     return result;
   }
