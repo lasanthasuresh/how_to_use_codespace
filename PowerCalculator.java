9160d9552754474fb11b5f23f5a48229d69fe099
Sahaswari Senanayaka <136144909+Sahaswari@users.noreply.github.com>
2023-09-22 02:25:18 +0000

    This is the rewrite the java code with clean code.

--- PowerCalculator.java
+++ PowerCalculator.java
@@ -5,20 +5,20 @@ public class PowerCalculator {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Enter the base number: ");
-    double base = scanner.nextDouble();
+    System.out.print("Enter the base number: ");
+    double baseValue = scanner.nextDouble();
 
-    System.out.println("Enter the exponent: ");
-    double exponent = scanner.nextDouble();
+    System.out.print("Enter the exponent: ");
+    double exponentValue = scanner.nextDouble();
 
-    double power = calculatePower(base, exponent);
-    System.out.println("The power of " + base + " raised to" + exponent + " is " +power );
+    double powerValue = calculatePower(baseValue, exponentValue);
+    System.out.print("The power of " + baseValue + " raised to" + exponentValue + " is " +powerValue );
   }
 
-  private static double calculatePower(double base, double exponent) {
+  private static double calculatePower(double baseValue, double exponentValue) {
     double result = 1;
-    for (int i = 0; i < exponent; i++) {
-      result *= base;
+    for (int i = 0; i < exponentValue; i++) {
+      result *= baseValue;
     }
     return result;
   }
