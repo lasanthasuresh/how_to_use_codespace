93d778ead5b085541117c418ca229a5f6b8b75a9
Razil Ahamed <139683252+razilahamed@users.noreply.github.com>
2023-09-22 02:17:38 +0000

    removed s

--- PowerCalculator.java
+++ PowerCalculator.java
@@ -5,10 +5,10 @@ public class PowerCalculator {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Enters the base: ");
+    System.out.println("Enter the base: ");
     double base = scanner.nextDouble();
 
-    System.out.println("Enters the exponent: ");
+    System.out.println("Enter the exponent: ");
     double exponent = scanner.nextDouble();
 
     double power = calculatePower(base, exponent);
