dcd485a654d1aecda33287bbdb5e2a45e70c63c3
mhdmusharraf <86569424+mhdmusharraf@users.noreply.github.com>
2023-09-22 01:36:38 +0000

    Any comment

--- PowerCalculator.java
+++ PowerCalculator.java
@@ -5,7 +5,7 @@ public class PowerCalculator {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Enter the base num: ");
+    System.out.println("Enter the base numb: ");
     double base = scanner.nextDouble();
 
     System.out.println("Enter the exp: ");
